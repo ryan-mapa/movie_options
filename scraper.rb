18dc7554e9cd6f4353a0d75e417bb8b7a31bf859
ryan mapa <ryan.m.mapa@gmail.com>
2017-11-16 15:21:13 -0800

    Configure links array

--- scraper.rb
+++ scraper.rb
@@ -7,10 +7,13 @@ chart = doc.at_css('.chart').children[5].children
 
 items = []
 data = []
+links2 = []
 
 chart.children.each_with_index do |child, idx|
   items.push(child.content.strip)
+  links2.push(child)
 end
+
 
 items.each {|item| data.push(item) unless item.empty?}
 
@@ -18,22 +21,28 @@ titles = []
 weekend = []
 gross = []
 
+data.each_with_index do |datum, idx|
+  if idx % 4 == 0
+    titles.push(datum.strip)
+  elsif idx % 4 == 1
+    weekend.push(datum.strip)
+  elsif idx % 4 == 2
+    gross.push(datum.strip)
+  end
+end
+
 # puts data
 testy = []
 chart.children.each {|el| testy.push(el.to_s)}
 
 long = testy.select {|el| el.strip.length > 200}
 
-# long.each do |el|
-#   puts "#{el}"
-#   puts
-# end
 
 links = []
 image_urls = []
 long.each do |el|
   if el.include?("titleColumn")
-    links.push(el.strip)
+    links.push("http://www.imdb.com/" + el.strip.split(" ")[3][6..-2])
   else
     image_urls.push(el.strip)
   end
