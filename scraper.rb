06833304443381ef5a051d6ea4644b1153dd0c42
ryan mapa <ryan.m.mapa@gmail.com>
2017-11-16 13:35:12 -0800

    Isolate chart data

--- scraper.rb
+++ scraper.rb
@@ -3,4 +3,20 @@ require 'Nokogiri'
 
 doc = Nokogiri::HTML(open("http://www.imdb.com/chart/boxoffice"))
 
-return doc
+chart = doc.at_css('.chart').children[5].children
+
+chart.children.each_with_index do |child, idx|
+  puts "HERER HERERE HEREREE #{idx}"
+  puts child
+end
+
+# puts chart.at_css('.ratingColumn')
+# chart.each do |movie|
+#   puts movie
+# end
+
+# chart.content.each {|el| puts el.strip}
+
+# puts chart.content
+
+# puts chart.chi
